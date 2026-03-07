8627b1d7587891e30f3f46c07f586d2fdfd4f0c1
yoonieaj <yoonie.jang@mail.utoronto.ca>
2026-03-07 09:07:13 -0500

    Changes to time bar

--- vis2/js/main_vis.js
+++ vis2/js/main_vis.js
@@ -176,43 +176,37 @@ function drawVis(data, planetsOnly) {
     let maxYear = d3.max(data, (d) => d.discovery_year);
     console.log("minyear", minYear)
     console.log("maxyear", maxYear)
-    
-    // the number of sections we wanna have? subject to change
-    const NUM_TIMEPERIODS = 5;
 
-    const sectionWidth = 800 / NUM_TIMEPERIODS;
-    let colors = ["#074665ff", "#1670a1ff", "#3dafd2ff", "#4fcaddff", "#6cdae0ff", "#a4f1f1ff"]
+    let timescale = d3.scaleTime()
+		.range([0, 800])
+		.domain([minYear, maxYear]);
 
-    // make it interactive
-    for (let i = 0; i <= NUM_TIMEPERIODS; i++) {
-        visSvg.append("rect")
-            .attr("id", "time-section-" + i)
-            .attr("x", 50 + i * sectionWidth)
-            .attr("y", 50)
-            .attr("width", sectionWidth)
-            .attr("height", 10)
-            .style("fill", colors[i]);
-        d3.select("#time-section-" + i).on("mouseover", function() {
-            d3.select(this).style("fill", "red")
-                .attr("height", 15)
-                .attr("y", 47.5);
-            for (let body of data) {
-                if ((body.discovery_year <= minYear + (i) * (maxYear - minYear) / NUM_TIMEPERIODS)
-                    && (body.discovery_year > minYear + (i-1) * (maxYear - minYear) / NUM_TIMEPERIODS)) {
-                    d3.select("#id" + body.name).style("fill", "red");
-                    console.log("body", body.name, body.discovery_year)
-                }
-            }
-        });
-        d3.select("#time-section-" + i).on("mouseout", function() {
-            d3.select(this).style("fill", colors[i])
-                .attr("height", 10)
-                .attr("y", 50);
-            for (let body of data) {
-                if (body.discovery_year <= minYear + (i) * (maxYear - minYear) / NUM_TIMEPERIODS) {
-                    d3.select("#id" + body.name).style("fill", null);
-                }
-            }
-        });
+    let timelineAxis = d3.axisBottom(timescale).scale(timescale)
+        .tickValues(timescale.ticks(5).concat(minYear, maxYear))
+        .tickFormat((d) => d3.format("d")(d))
+        .tickSize(10);
+
+    let timelineBrush = d3.brushX()
+		.extent([[0, -20], [800, 0]])
+		.on("brush", (event) => brushed(data,timescale, event));
+
+    visSvg.append("g")
+		.attr("class", "timescale")
+		.attr("transform", "translate(100, 30)")
+        .call(timelineBrush)
+        .call(timelineAxis);
+}
+
+function brushed(data, timescale) {
+    // react to brushed event
+    let selection = d3.brushSelection(d3.select(".timescale").node());
+    console.log("selection", selection)
+    for (let body of data) {
+        let x = timescale(body.discovery_year);
+        if (selection && x >= selection[0] && x <= selection[1]) {
+            d3.select("#id" + body.name).style("fill", "red");
+        } else {
+            d3.select("#id" + body.name).style("fill", null);
+        }
     }
 }
